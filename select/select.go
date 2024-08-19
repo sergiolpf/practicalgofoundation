306feb83b951ff09b6f835582c644902da39cb3b
Sergio Filho <sergiolpf@gmail.com>
2024-08-19 15:03:22 +1200

    adding context to select.go

--- select/select.go
+++ select/select.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
@@ -18,12 +19,15 @@ func main() {
 		ch2 <- 2
 	}()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+
 	select {
 	case val := <-ch1:
 		fmt.Println("ch1:", val)
 	case val := <-ch2:
 		fmt.Println("ch2:", val)
-	case <-time.After(5 * time.Millisecond):
+	case <-ctx.Done():
 		fmt.Println("timeout")
 	}
 
